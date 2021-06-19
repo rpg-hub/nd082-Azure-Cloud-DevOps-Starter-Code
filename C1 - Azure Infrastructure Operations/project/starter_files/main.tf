3ed28ca30ce743721d5b43c45ccf159e16865007
Rajeev <rp.neuli@yahoo.com>
2021-06-19 12:38:51 -0500

    Update main.tf

--- C1 - Azure Infrastructure Operations/project/starter_files/main.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/main.tf
@@ -54,17 +54,31 @@ resource "azurerm_network_security_group" "project1" {
   # source_address_prefix = ["10.0.0.0/22"]  #was giving err
 
   # rule to allow from only within subnet, covers ssh.
-  security_rule {
-    name                       = "inboundAccess"
+  security_rule [
+    {
+    name                       = "inboundAccessSSH"
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "*"
+    destination_port_range     = "22"
     source_address_prefix      = "10.0.0.0/24"
     destination_address_prefix = "*"
+    description            = "description-ssh"
+  },
+  {
+    name                       = "DenyAllInbound"
+    priority                   = 65500
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "10.0.0.0/24"
   }
+  ]
   
   tags = {
     ND = "1"
